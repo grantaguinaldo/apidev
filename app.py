b845cc63770a847bb67897cbdeb76877cc45a0be
Grant T. Aguinaldo <grant.aguinaldo@gmail.com>
2020-05-20 15:00:21 -0700

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def baidcheck(baid):
     
     'BA_ID': baid,
     'GNN_ID': randint(10000, 99999),
-    'IS ELIGIBLE': int(round(random.uniform(0, 1))),
+    'IS_ELIGIBLE': int(round(random.uniform(0, 1))),
     'BA_OPEN_DT': '2019-01-01',
     'QUERY_ID': str(uuid.uuid4())}
     
@@ -31,20 +31,11 @@ def billingdata(baid):
     
     'GNN_ID': randint(10000, 99999),
     'BA_ID': baid,
-    'SVC_ZIP': randint(10000, 99999),
-    'CEC_ZONE': randint(10, 99),
-    '2018_BASELOAD_THMS_WINTER_2018_THMS': randint(1, 99999),
-    'WINTER_2018_2019_THMS': randint(1, 99999),
-    'TOTAL_THMS': randint(1, 99999),
-    '2018_ANNUAL_WINTER_2018_BASE_RATIO': random.uniform(0, 1),
-    'WINTER_2018_2019_TOTAL_RATIO': random.uniform(0, 1),
-    'IS_WINTER_2018_THMS_BETWEEN_50_70_PCT': int(round(random.uniform(0, 1))),
-    'IS_WINTER_2018_THMS_ABOVE_70_PCT': int(round(random.uniform(0, 1))),
-    'IS_2018_BASELOAD_THMS_50_70_PCT': int(round(random.uniform(0, 1))),
-    'IS_2018_BASELOAD_THMS_ABOVE_70_PCT': int(round(random.uniform(0, 1))),
-    'IS_WINTER_2018_BASE_RATIO_50_70_PCT': int(round(random.uniform(0, 1))),
-    'IS_WINTER_2018_BASE_RATIO_ABOVE_70_PCT': int(round(random.uniform(0, 1))),
-    'BA_OPEN_DT': '2019-01-01',
+    'AS_USED_YR_MO': '2020-05',
+    'AS_BILLED_YR_MO': '2020-06',
+    'TOT_THMS_BILLED': randint(10, 99999),
+    'BILLING_ST_DT': '2020-04-01',
+    'BILLING_END_DT': '2020-05-01',
     'QUERY_ID': str(uuid.uuid4())}
     
     return jsonify(payload_customer)
